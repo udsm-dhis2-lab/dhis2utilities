43a6dedefe47fad8724c42c0de830250c93cfe51
josephatj <josephatjulius24@gmail.com>
2017-10-04 06:23:18 +0300

    Formatting the datasets from backup and saving added

--- create_form_attributes.js
+++ create_form_attributes.js
@@ -49,41 +49,40 @@ function storeDataSets(headers) {
             })
     });
     promise.then(function(formArr) {
-        localStorage.setItem('indicatorsData',JSON.stringify(formArr));
+        localStorage.setItem('formStore',JSON.stringify(formArr));
     });
 }
 
 // backup the dataSets and return their names
 function retrieveDataSetsObjectsAndStoreBackup() {
     var filesArr =[]; // to hold file Names
-    var arr = localStorage.getItem('indicatorsData');
+    var arr = localStorage.getItem('formStore');
     JSON.parse(arr).forEach(function (dataSet) {
-        var content = dataSet.dataEntryForm.htmlCode;
-        var filename = 'originalHtmls/'+dataSet.name+'.html';
-        filesArr.push(filename);
-          fs.writeFile(filename, content, function (err) {
-              console.log('backup made for ' + filename);
-          });
+        fs.readFile('scripts/script.html', 'utf8', getScript);
+        function getScript(error, data) {
+            var calculatingScript = '';
+            $ = cheerio.load('' + data + '');
+            $('script').each(function (index, element) {
+                calculatingScript += $.html(element);
+            });
+            var filename = 'originalHtmls/'+dataSet.name+'.html';
+            var content = '<section>\r\n<input dataSetName ="dataSetName" name = "'+ filename +'" />\r\n'+calculatingScript+dataSet.dataEntryForm.htmlCode+'</section>';
+            filesArr.push(filename);
+            fs.writeFile(filename, content, function (err) {
+                console.log('backup made for ' + filename);
+            });
+            fs.readFile(filename, 'utf8', formatHtml);
+        }
     });
-    return filesArr;
 }
 
-function readAndFormatHtmls(fileNamesArr) {
-    for (var key in fileNamesArr) {
-        fs.readFile(fileNamesArr[key], 'utf8', formatHtml)
-    }
-}
 
-// fs.readFile('htmls/original.html', 'utf8', formatHtml);
-
-function formatHtml(err, data) {
-
+function formatHtml(error, data) {
     $ = cheerio.load('' + data + '');
     var totalsArr = []; var entryFieldsArr = []; var indStr = '';
     $('input[name="entryfield"]').each(function (i, element) {
         entryFieldsArr.push(element.attribs.id);
     });
-    console.log(entryFieldsArr);
     $('input[name="total"]').each(function (i, element) {
         totalsArr.push(element.attribs.id);
     });
@@ -91,145 +90,146 @@ function formatHtml(err, data) {
     $('input[name="indicator"]').each(function (i, element) {
         indStr += element.attribs.id.substr(9) +',';
     });
+
     var calledIndArr = [];
 
+        var Path = "/api/indicators.json?paging=false&filter=id:in:[" + indStr.substr(0, indStr.length - 1) + "]&fields=id,numerator,denominator";
+        console.log(Path);
+        var Promise = require('promise');
+        var promise = new Promise(function (resolve, reject) {
+            request({
+                    headers: headers,
+                    uri: URL + Path,
+                    method: 'GET'
+                },
+                function (error, response, body) {
+                    if (!error && response.statusCode == 200) {
+                        var res = JSON.parse(body).indicators;
+                        res.forEach(function (indicator) {
+                            calledIndArr.push(indicator);
+                            console.log(indicator)
+                            resolve(calledIndArr);
+                        })
+                    } else {
+                        if (response) {
+                            console.log(response.statusCode + ":", JSON.stringify(error));
+                            reject();
+                        } else {
+                            console.log(response);
+                        }
+                    }
+                })
+        });
+        promise.then(function(calledIndArr) {
+            localStorage.setItem('indicatorsData',JSON.stringify(calledIndArr));
+        });
+    var tmpData = {};
+    for (var index in totalsArr) {
+        var arrIndId = '';
+        entryFieldsArr.forEach(function (entryId, j) {
+            if (entryId.split("-")[0] === totalsArr[index].substr(5)) {
+                arrIndId += '#{'+entryId.split("-")[0] + "." + entryId.split("-")[1]+'},';
+            }
+        });
+        tmpData[totalsArr[index]] = '('+arrIndId.substring(0, arrIndId.length - 1).split(",").join("+")+')';
+    }
 
-    //     var Path = "/api/indicators.json?paging=false&filter=id:in:[" + indStr.substr(0, indStr.length - 1) + "]&fields=id,numerator,denominator";
-    //     console.log(Path)
-    //     var Promise = require('promise');
-    //     var promise = new Promise(function (resolve, reject) {
-    //         request({
-    //                 headers: headers,
-    //                 uri: URL + Path,
-    //                 method: 'GET'
-    //             },
-    //             function (error, response, body) {
-    //                 if (!error && response.statusCode == 200) {
-    //                     var res = JSON.parse(body).indicators;
-    //                     console.log(JSON.parse(body).indicators.length);
-    //                     res.forEach(function (indicator) {
-    //                         calledIndArr.push(indicator);
-    //                         resolve(calledIndArr);
-    //                     })
-    //                 } else {
-    //                     if (response) {
-    //                         console.log(response.statusCode + ":", JSON.stringify(error));
-    //                         reject();
-    //                     } else {
-    //                         console.log(response);
-    //                     }
-    //                 }
-    //             })
-    //     });
-    //     promise.then(function(calledIndArr) {
-    //         console.log('IND '+calledIndArr);
-    //         localStorage.setItem('indicatorsData',JSON.stringify(calledIndArr));
-    //     });
-    // var tmpData = {};
-    // for (var index in totalsArr) {
-    //     var arrIndId = '';
-    //     entryFieldsArr.forEach(function (entryId, j) {
-    //         if (entryId.split("-")[0] === totalsArr[index].substr(5)) {
-    //             arrIndId += '#{'+entryId.split("-")[0] + "." + entryId.split("-")[1]+'},';
-    //         }
-    //     });
-    //     tmpData[totalsArr[index]] = '('+arrIndId.substring(0, arrIndId.length - 1).split(",").join("+")+')';
-    // }
+  $('section').each(function(i, elem) {
+      //get indicators from local storage
+      var arr = localStorage.getItem('indicatorsData');
+      var indAggregations = {};
+      // console.log(tmpData);
+      for (var counter = 0; counter < Object.keys(JSON.parse(arr)).length; counter++) {
+          if (JSON.parse(arr)[counter].numerator.split("+")[0].length < 18) {
+              var originalNumerator = JSON.parse(arr)[counter].numerator;
+              for (var elementCounter = 0; elementCounter < JSON.parse(arr)[counter].numerator.split("+").length; elementCounter++) {
+                  for (var index in tmpData) {
+                      if (index.substr(5) === JSON.parse(arr)[counter].numerator.split("+")[elementCounter].replace("#{", "").replace("}", "")) {
+                          originalNumerator = originalNumerator.replace(JSON.parse(arr)[counter].numerator.split("+")[elementCounter], tmpData[index].replace("(","").replace(")",""));
+                      }
+                  }
+              }
+              // Recheck if there is still dataelements in the indicator definition
+              var finalRes = originalNumerator;
+              for (var orCounter =0; orCounter < originalNumerator.replace("(","").replace(")","").split("+").length; orCounter++) {
+                  var newIndDefStr = '';
+                  // console.log("CHECK "+originalNumerator.replace("(","").replace(")","").split("+")[orCounter].replace("#{","").replace("}",""))
+                  if (originalNumerator.replace("(","").replace(")","").split("+")[orCounter].length < 18) {
+                      $('input[name="entryfield"]').each(function (i, field) {
+                          // console.log(field.attribs.id.split("-")[0]+ "  "+i);
+                          if (field.attribs.id.split("-")[0] === originalNumerator.replace("(","").replace(")","").split("+")[orCounter].replace("#{","").replace("}","").replace(" ","").replace(" ","")) {
+                              newIndDefStr += "#{"+field.attribs.id.replace("-",".").replace("-val","")+"}+";
+                          }
+                      });
+                      finalRes  = finalRes.replace(originalNumerator.replace("(","").replace(")","").split("+")[orCounter],newIndDefStr.substr(0,newIndDefStr.length - 1));
+                  }
+              }
+              indAggregations[JSON.parse(arr)[counter].id] = '('+finalRes+')';
+          } else {
+              //remove input ids which are not in the form
+              var check =false;
+              var beforeReplacement = JSON.parse(arr)[counter].numerator;
+              for (var idCount=0; idCount < beforeReplacement.split("+").length; idCount++) {
+                  var inputId = beforeReplacement.split("+")[idCount].replace("#{","").replace("}","").replace(" ","").replace(" ","").replace(".","-")+"-val";
+                  if ($('input[id="' + inputId + '" ]').length > 0) {
+                      // the id is in the html file
+                  } else {
+                      // id not in the html file
+                      check = true;
+                      var AfterReplacement = beforeReplacement.replace(beforeReplacement.split("+")[idCount]+"+","").replace("+"+beforeReplacement.split("+")[idCount],"");
+                      beforeReplacement = AfterReplacement;
+                      console.log("The id which is not in the inputs but in the indicator formula from api is "+JSON.parse(arr)[counter].numerator.split("+")[idCount].replace("#{","").replace("}","").replace(".","-")+"-val");
+                  }
+              }
+              indAggregations[JSON.parse(arr)[counter].id] = '('+beforeReplacement+ ')/('+JSON.parse(arr)[counter].denominator+')';
+              // console.log("THAT "+beforeReplacement);
+          }
+      }
+    console.log(indAggregations);
+      $('input[dataelementid]').each(function (index, val) {
+         //  var test = $(val).attr('name');
+          for (var aggr in tmpData){
+              if ($(val).attr('id') === aggr){
+                  $(val).attr('id', tmpData[aggr]);
+              }
+          }
+          $(val).removeAttr('dataelementid');
+          $(val).removeAttr('value');
+          $(val).attr('name', 'indicatorFormula');
+          $(val).attr('disabled','disabled');
+      });
+      $('input[indicatorid]').each(function (index, val) {
+          for (var indAggr in indAggregations){
+              if ($(val).attr('id').substr(9) === indAggr){
+                  $(val).attr('id', indAggregations[indAggr]);
+              }
+          }
+          $(val).removeAttr('indicatorid');
+          $(val).removeAttr('value');
+          $(val).attr('name', 'indicatorFormula');
+          $(val).attr('disabled','disabled');
+      });
 
-  // $('section').each(function(i, elem) {
-  //     //get indicators from local storage
-  //     var arr = localStorage.getItem('indicatorsData');
-  //     var indAggregations = {};
-  //     // console.log(tmpData);
-  //     for (var counter = 0; counter < Object.keys(JSON.parse(arr)).length; counter++) {
-  //         if (JSON.parse(arr)[counter].numerator.split("+")[0].length < 18) {
-  //             var originalNumerator = JSON.parse(arr)[counter].numerator;
-  //             for (var elementCounter = 0; elementCounter < JSON.parse(arr)[counter].numerator.split("+").length; elementCounter++) {
-  //                 for (var index in tmpData) {
-  //                     if (index.substr(5) === JSON.parse(arr)[counter].numerator.split("+")[elementCounter].replace("#{", "").replace("}", "")) {
-  //                         originalNumerator = originalNumerator.replace(JSON.parse(arr)[counter].numerator.split("+")[elementCounter], tmpData[index].replace("(","").replace(")",""));
-  //                     }
-  //                 }
-  //             }
-  //             // Recheck if there is still dataelements in the indicator definition
-  //             var finalRes = originalNumerator;
-  //             for (var orCounter =0; orCounter < originalNumerator.replace("(","").replace(")","").split("+").length; orCounter++) {
-  //                 var newIndDefStr = '';
-  //                 // console.log("CHECK "+originalNumerator.replace("(","").replace(")","").split("+")[orCounter].replace("#{","").replace("}",""))
-  //                 if (originalNumerator.replace("(","").replace(")","").split("+")[orCounter].length < 18) {
-  //                     $('input[name="entryfield"]').each(function (i, field) {
-  //                         // console.log(field.attribs.id.split("-")[0]+ "  "+i);
-  //                         if (field.attribs.id.split("-")[0] === originalNumerator.replace("(","").replace(")","").split("+")[orCounter].replace("#{","").replace("}","").replace(" ","").replace(" ","")) {
-  //                             newIndDefStr += "#{"+field.attribs.id.replace("-",".").replace("-val","")+"}+";
-  //                         }
-  //                     });
-  //                     finalRes  = finalRes.replace(originalNumerator.replace("(","").replace(")","").split("+")[orCounter],newIndDefStr.substr(0,newIndDefStr.length - 1));
-  //                 }
-  //             }
-  //             indAggregations[JSON.parse(arr)[counter].id] = '('+finalRes+')';
-  //         } else {
-  //             //remove input ids which are not in the form
-  //             var check =false;
-  //             var beforeReplacement = JSON.parse(arr)[counter].numerator;
-  //             for (var idCount=0; idCount < beforeReplacement.split("+").length; idCount++) {
-  //                 var inputId = beforeReplacement.split("+")[idCount].replace("#{","").replace("}","").replace(" ","").replace(" ","").replace(".","-")+"-val";
-  //                 if ($('input[id="' + inputId + '" ]').length > 0) {
-  //                     // the id is in the html file
-  //                 } else {
-  //                     // id not in the html file
-  //                     check = true;
-  //                     var AfterReplacement = beforeReplacement.replace(beforeReplacement.split("+")[idCount]+"+","").replace("+"+beforeReplacement.split("+")[idCount],"");
-  //                     beforeReplacement = AfterReplacement;
-  //                     console.log("The id which is not in the inputs but in the indicator formula from api is "+JSON.parse(arr)[counter].numerator.split("+")[idCount].replace("#{","").replace("}","").replace(".","-")+"-val");
-  //                 }
-  //             }
-  //             indAggregations[JSON.parse(arr)[counter].id] = '('+beforeReplacement+ ')/('+JSON.parse(arr)[counter].denominator+')';
-  //             // console.log("THAT "+beforeReplacement);
-  //         }
-  //     }
-  //   console.log(indAggregations);
-  //     $('input[dataelementid]').each(function (index, val) {
-  //        //  var test = $(val).attr('name');
-  //         for (var aggr in tmpData){
-  //             if ($(val).attr('id') === aggr){
-  //                 $(val).attr('id', tmpData[aggr]);
-  //             }
-  //         }
-  //         $(val).removeAttr('dataelementid');
-  //         $(val).removeAttr('value');
-  //         $(val).attr('name', 'indicatorFormula');
-  //         $(val).attr('disabled','disabled');
-  //     });
-  //     $('input[indicatorid]').each(function (index, val) {
-  //         for (var indAggr in indAggregations){
-  //             if ($(val).attr('id').substr(9) === indAggr){
-  //                 $(val).attr('id', indAggregations[indAggr]);
-  //             }
-  //         }
-  //         $(val).removeAttr('indicatorid');
-  //         $(val).removeAttr('value');
-  //         $(val).attr('name', 'indicatorFormula');
-  //         $(val).attr('disabled','disabled');
-  //     });
-  //
-  //   var id = $(elem).attr('id'),
-  //       filename = 'htmls/new.html',
-  //       content = $.html(elem);
-  //     // console.log(indAggregations);
-  //   fs.writeFile(filename, content, function (err) {
-  //       console.log('Written html to ' + filename);
-  //   });
-  // });
+      var formName = '';
+      $('input[dataSetName = "dataSetName"]').each(function (index, obj) {
+          formName += obj.attribs.name
+      });
+
+        filename = formName.replace('originalHtmls','newHtmls');
+        content = $.html(elem);
+      // console.log(indAggregations);
+    fs.writeFile(filename, content, function (err) {
+        console.log('Written html to ' + filename);
+    });
+  });
 }
 
 function readAndFormat() {
     // Store DataSets
     storeDataSets(headers);
+    
     // Retrieve the dataSets and their names
-    var fileNamesArr = retrieveDataSetsObjectsAndStoreBackup();
-
-    // read and For
-    readAndFormatHtmls(fileNamesArr);
+    retrieveDataSetsObjectsAndStoreBackup();
 }
 
 readAndFormat();
