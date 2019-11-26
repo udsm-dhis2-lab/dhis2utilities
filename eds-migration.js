0844056a8e877dc41fc3ae6c12ba54e47848c7c4
josephatJ <josephatjulius24@gmail.com>
2019-11-26 10:55:59 +0300

    add testing request

--- eds-migration.js
+++ eds-migration.js
@@ -1,13 +1,45 @@
-const URL = process.argv[2];
-const credentialsSource = process.argv[3];
-const typeOfActivity = process.argv[4];
-const request = require("request");
-const http = require("http");
-const https = require("https");
-const tokenSource = btoa();
-const credentialsDestination = "";
+require("dotenv").config();
+const axios = require("axios");
+// const credentialsSource = process.argv[3];
+const tokenSource = new Buffer(process.env.CREDENTIALS_SOURCE).toString(
+  "base64"
+);
+const tokenDestination = new Buffer(
+  process.env.CREDENTIALS_DESTINATION
+).toString("base64");
+const SOURCE_BASE_URL = process.env.SOURCE_BASE_URL;
+const DESTINATION_BASE_URL = process.env.DESTINATION_BASE_URL;
+console.log(tokenSource);
+console.log(tokenDestination);
 let headerConfigsSource = {
   headers: {
-    Authorization: "Bearer " + tokenSource
+    Authorization: "Basic " + tokenSource
   }
 };
+
+let headerConfigsDestination = {
+  headers: {
+    Authorization: "Basic " + tokenDestination
+  }
+};
+const programs = [
+  "RwVrL1Y8RTH",
+  "CT0TNl30rld",
+  "Z4szHfJebFL",
+  "jYsHdmTJNVh",
+  "go4MncVomkQ",
+  "R8APevjOH0o"
+];
+
+axios
+  .get(DESTINATION_BASE_URL + "api/users", {
+    headers: headerConfigsDestination.headers
+  })
+  .then(function(response) {
+    // handle success
+    console.log(response.data);
+  })
+  .catch(function(error) {
+    // handle error
+    console.log(error);
+  });
